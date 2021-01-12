c91a8a559a6d0d2b6a12763e7a1f0ecc8e38db12
saliouvj <m.saliou90@gmail.com>
2021-01-12 09:34:18 +0100

    end

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+console.log("_1_");
+//1 
+// let nbMax = parseInt(prompt('Nombre?'));
+// let compt = 0; 
+
+// while (compt <= nbMax) {
+//         console.log(compt);
+//         compt++
+// }
+
+console.log("_2.1_");
+//2.1
+
+// let arret = 4;
+// let compt2 = 0;
+
+// while (compt2<=arret) {
+//     console.log(compt2);
+//     compt2++
+// }
+
+console.log("_2.2_");
+//2.2
+
+// let arret = 6;
+// let compt = 3;
+
+// while (compt <= arret) {
+//     console.log(compt);
+//     compt++      // compt = compt +1
+// }
+
+console.log("_2.3_");
+//2.3
+
+let arret = 8;
+let compt = 0;
+
+while (compt <= arret) {
+    console.log(compt);
+    compt += 2;     // compt = compt +2
+}
