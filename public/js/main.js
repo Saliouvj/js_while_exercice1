e5f85410ac921caec18b9cbd59e49004b89c1f42
saliouvj <m.saliou90@gmail.com>
2021-01-12 10:48:52 +0100

    end

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,115 @@
-console.log("_1_");
-//1 
-// let nbMax = parseInt(prompt('Nombre?'));
-// let compt = 0; 
+// 1.1
+// let valeur = prompt("Entrez une valeur: ")
+// valeur = valeur.trim();
 
-// while (compt <= nbMax) {
+// while (valeur == "") {
+//     valeur = prompt(`Vous n'avez rien ecrit. Entrez une valeur ?`)
+// };
+
+// valeur = valeur.replace(/\s+/, '')
+// while (valeur.length == 0) {
+//     valeur = prompt(`Vous n'avez rien ecrit. Entrez une valeur ?`)
+//     valeur = valeur.replace(/\s+/, '')
+
+// };
+
+// while(valeur.length <= 0){
+//     valeur = prompt("Entrez votre val"); 
+//     valeur = valeur.trim();
+
+// }
+// console.log(valeur);
+
+//1.2
+// let chiffre = parseInt(prompt(`Entrez un chiffre plus grand que 10 ?`));
+
+// switch (isNaN(chiffre)) {
+//     case true:
+
+//         break;
+//     case false:
+
+//         break;
+//     default:
+//         break;
+// }
+
+// if (!isNaN(chiffre)) {
+//     while (chiffre < 10) {
+//         chiffre = parseInt(prompt(`${chiffre} est plus petit que 10. Entrez un autre chiffre?`));
+//     }
+// } else {
+//     alert(`ce n'est pas une nombre réasayez !`)
+// }
+
+// while (chiffre < 10) {
+//     chiffre = parseInt(prompt(`${chiffre} est plus petit que 10. Entrez un autre chiffre?`));
+// }
+// console.log(chiffre);
+
+//2.1 
+// let compt = 0;
+// while (compt <= 11) {
+//     if (compt % 2 == 1) {
 //         console.log(compt);
-//         compt++
+//     };
+//     compt++
 // }
 
-console.log("_2.1_");
-//2.1
-
-// let arret = 4;
-// let compt2 = 0;
-
-// while (compt2<=arret) {
-//     console.log(compt2);
-//     compt2++
-// }
-
-console.log("_2.2_");
 //2.2
+// let prenom = prompt('Quel est ton prénom ?');
+// let prenomTaille = prenom.length;
+// let compt = 0;
 
-// let arret = 6;
-// let compt = 3;
-
-// while (compt <= arret) {
-//     console.log(compt);
-//     compt++      // compt = compt +1
+// while (compt < prenomTaille) {
+//     console.log(prenom);
+//     compt++
 // }
 
-console.log("_2.3_");
+
 //2.3
+// while (compt < prenomTaille) {
+//     console.log(prenom.charAt(compt));
+//     compt++
+// }
 
-let arret = 8;
-let compt = 0;
 
-while (compt <= arret) {
-    console.log(compt);
-    compt += 2;     // compt = compt +2
+//3.1
+// let prenom = prompt('Quel est ton prénom ?');
+// let prenomTaille = prenom.length;
+// let compt = 0;
+
+// while (compt < prenomTaille) {
+//     if (compt % 2 == 1) {
+//         console.log(`${prenom.charAt(compt).toUpperCase()} =>  à l'indice ${compt}`);
+//     } else {
+//         console.log(`${prenom.charAt(compt).toLowerCase()} =>  à l'indice ${compt}`);
+//     }
+//     compt++
+// }
+
+//3.2
+// let calcul = prompt("Quel est ton calcul ?");   //3.5 * 2.5
+// let condition = true; 
+
+// while (condition == true) {
+//     console.log(Math.ceil(eval(calcul)));
+
+//     let stop = prompt('Veux-tu arreter ?')
+//     if (stop == "oui") {
+//         condition = false 
+//         console.log('Merci.');
+//     } else {
+//         calcul = prompt("Quel est ton calcul ?");   //3.5 * 2.5
+//     }
+// }
+
+//3.3
+let prenom = prompt('ton prenom');
+
+while (prenom.length <= 5 ) {
+    prenom = prompt('prenom trop court,  recommencer')
+}
+if (prenom.length >=5) {
+    console.log(prenom.toUpperCase());
 }
